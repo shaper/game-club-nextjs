2e90788b067f76fe9da25a44d10806b4420a4fcc
Walter Korman <lemurware@gmail.com>
2024-09-08 13:01:18 -0700

    Add history display with backing data file.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['steamcdn-a.akamaihd.net', 'images.ctfassets.net', 'm.media-amazon.com', 'image.api.playstation.com', 'static-cdn.jtvnw.net', 'cdn1.epicgames.com'],
+  },
+};
 
 export default nextConfig;
